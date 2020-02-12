e8265563ae6b58765fb1be52321c3059341e59df
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2020-02-12 22:23:24 +0530

    AoC 2016 - Day 3 - WASM

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 mod utils;
+mod data;
 
 use wasm_bindgen::prelude::*;
 
@@ -8,12 +9,64 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+pub struct TriangleSolver{
+    unsolved:String,
+    solved:Vec<i32>,
+    count:i32,
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, {{project-name}}!");
+impl TriangleSolver{
+
+    //Gets an emtpy struct
+    pub fn new()->TriangleSolver{
+        TriangleSolver{
+            unsolved:data::get_triangle_data(),
+            solved:Vec::new(),
+            count:0,
+        }
+    }
+
+    //Read dataset
+
+    pub fn count_triangles_horizontally(&mut self) -> i32{
+        let mut count = 0;
+        for sides_list in self.unsolved.lines(){
+            let sides:Vec<i32> = sides_list
+                .split_whitespace()
+                .map(|e| e.parse().unwrap())
+                .collect();
+            let sides_cloned = sides.clone();
+
+            if self.is_valid_triangle(sides) == true{
+                self.solved.extend_from_slice(&sides_cloned.as_slice()[0..]);
+                self.count+=1;
+                count+=1;
+            };
+        }
+        count
+    }
+
+    pub fn get_solved(&self) -> *const i32{
+        self.solved.as_ptr()
+    }
+
+    pub fn get_size(&self) -> usize{
+        self.solved.len()
+    }
+
+    fn is_valid_triangle(&self,sides:Vec<i32>)->bool{
+        for i in 0..sides.len(){
+            let l=&sides[..i];
+            let r=&sides[i+1..];
+            let merged = [l,r].concat();
+    
+            if sides[i] >= merged.iter().sum(){
+                return false;
+            }
+        }
+        return true
+    }
 }
